d50f91fa92e71244c7555efc92b9569d245e6c5d
lujainjabbour1 <105502726+lujainjabbour1@users.noreply.github.com>
2022-05-13 17:20:17 +0300

    Add files via upload

---
+++ Q 1_A.py
@@ -0,0 +1,10 @@
+
+student = ['Lujain','Nada','Layal','Jafar','Bahaa','Leen','Moner']
+
+name = input ('enter the  student name :')
+
+if name in student :
+   print('This name is a gradual student')
+   
+else:
+    print('The name is not gradual student')
